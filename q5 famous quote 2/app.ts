8f7adb87e196149c8865ee84cdc2209f6f18a4d8
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-15 23:31:09 +0500

    Add files via upload

---
+++ q5 famous quote 2/app.ts
@@ -0,0 +1,4 @@
+let famousperson: string = "Albert Einstien";
+let quote: string = " A person who never made a mistake never tried anything new";
+let message =`${famousperson}once said, ${quote}`;
+console.log(message);
