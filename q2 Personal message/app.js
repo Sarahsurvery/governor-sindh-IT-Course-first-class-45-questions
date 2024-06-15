ba198368ff173fd63ecdf3c5a175d83977acb8e8
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-15 23:17:11 +0500

    Add files via upload

---
+++ q2 Personal message/app.js
@@ -0,0 +1,2 @@
+var personName = "faizan";
+console.log("Hello ".concat(personName, ", How are you? would you like to learn programming language? "));
---
+++ q2 Personal message/app.ts
@@ -0,0 +1,2 @@
+let personName: string= "faizan";
+console.log(`Hello ${personName}, How are you? would you like to learn programming language? `)
