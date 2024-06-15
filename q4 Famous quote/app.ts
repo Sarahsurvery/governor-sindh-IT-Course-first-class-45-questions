f34ebdeb917dafce9becb1a291db63ef9b300c42
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-15 23:25:55 +0500

    Add files via upload

---
+++ q4 Famous quote/app.ts
@@ -0,0 +1,4 @@
+let famousquote: string = "A person who never made a mistake never tried anything new";
+let authour: string = "Albert Einstein";
+
+console.log(`${authour} once said, "${famousquote}`);
