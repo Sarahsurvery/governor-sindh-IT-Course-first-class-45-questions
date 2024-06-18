87dead7c29c384218ad6d9e9e73f56298c672a59
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 23:14:13 +0500

    Add files via upload

---
+++ q35 Animals/app.js
@@ -0,0 +1,5 @@
+var animals = ["dog", "cat", "cow", "cammel"];
+animals.forEach(function (animal) {
+    console.log("A ".concat(animal, " would a great pet"));
+});
+console.log("These animals would a great pet!");
---
+++ q35 Animals/app.ts
@@ -0,0 +1,7 @@
+let animals: string []= ["dog","cat","cow","cammel"];
+
+animals.forEach(animal =>{
+    console.log(`A ${animal} would a great pet`);
+});
+
+console.log(`These animals would a great pet!`);
