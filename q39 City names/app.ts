9ba613e935a25c21aebb03e0f8c5a68351b7ecb3
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 23:29:02 +0500

    Add files via upload

---
+++ q39 City names/app.ts
@@ -0,0 +1,13 @@
+function city_country(city: string, country: string){
+    //return ` "${city}, ${country}"
+    console.log(`"${city}, ${country}"`)
+}
+
+city_country("Karachi", "Pakistan");
+
+let mycities = city_country("Karachi","Pakistan");
+console.log(mycities);
+
+console.log(city_country("Dubai","UAE"));
+console.log(city_country("london","UK"));
+console.log(city_country("Makkah","SaudiArabia"));
