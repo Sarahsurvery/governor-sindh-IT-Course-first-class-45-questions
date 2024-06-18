3373ebfe224dd8874ca943ef5a81741c66aacdd4
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 20:39:02 +0500

    Add files via upload

---
+++ q20 Think of some thing/app.js
@@ -0,0 +1,6 @@
+var language = ["english", "urdu", "sindhi", "punjabi"];
+console.log("list of languages : ");
+for (var _i = 0, language_1 = language; _i < language_1.length; _i++) {
+    var top_1 = language_1[_i];
+    console.log(top_1);
+}
---
+++ q20 Think of some thing/app.ts
@@ -0,0 +1,9 @@
+let language:string[]=["english","urdu","sindhi","punjabi"];
+
+console.log("list of languages : ");
+
+for(let top of language){
+
+    console.log(top);
+    
+}
