4c20780d6d0f18a562502d1563f69d184c55c3bc
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 22:06:22 +0500

    Add files via upload

---
+++ q29 Favourite fruit/app.js
@@ -0,0 +1,19 @@
+var favourite_fruit = ["mango", "pomegranate", " grapes", "apple", "banana"];
+if (favourite_fruit.includes('grapes')) {
+    console.log('do you want to eat?');
+}
+if (favourite_fruit.includes('mango')) {
+    console.log('I eat mango!');
+}
+if (favourite_fruit.includes('pomegranate')) {
+    console.log('I eat pomegranate!');
+}
+if (favourite_fruit.includes('grapes')) {
+    console.log('I eat grapes!');
+}
+if (favourite_fruit.includes('mango')) {
+    console.log('I eat mango!');
+}
+if (favourite_fruit.includes('banana')) {
+    console.log('I like banana!');
+}
---
+++ q29 Favourite fruit/app.ts
@@ -0,0 +1,25 @@
+let favourite_fruit : string [] = ["mango", "pomegranate", " grapes","banana"];
+
+if (favourite_fruit.includes('grapes')){
+    console.log('do you want to eat?');
+}
+
+if (favourite_fruit.includes('mango')){
+    console.log('I eat mango!');
+}
+
+if (favourite_fruit.includes('pomegranate')){
+    console.log('I eat pomegranate!');
+}
+
+if (favourite_fruit.includes('grapes')){
+    console.log('I eat grapes!');
+}
+
+if (favourite_fruit.includes('mango')){
+    console.log('I eat mango!')
+}
+
+if (favourite_fruit.includes('banana')){
+    console.log('I like banana!')
+}
