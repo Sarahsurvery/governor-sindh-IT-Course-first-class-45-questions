860d57e947318f7deb33660a0b9add4a30d567b4
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 20:27:04 +0500

    Add files via upload

---
+++ q16 More_guest/app.ts
@@ -0,0 +1,17 @@
+let guest_list: string []=["faizan","waniya","hira","bilal"];
+// for (let i =0; i < guest_list.length; i++){
+//     console.log(' Respected friends ' + guest_list[i]+'\n please come to my party at home,\n thank you\n');
+// }
+
+let not_present: string = 'faizan';
+let new_guest: string = 'bilal';
+guest_list[2] =new_guest;
+for (let i =0; i < guest_list.length; i++){
+    console.log(' Respected friends ' + guest_list[i]+'\n please come to my party at home,\n thank you\n');
+}
+//console.log(`Mr. ${not_present} will not come tomorrow party`)
+
+guest_list.unshift('sarah','umer','asiya');
+for (let i =0; i < guest_list.length; i++){
+    console.log(' Respected friends ' + guest_list[i]+'\n please come to my home, we celebrate eid festival, \n thank you\n');
+}
