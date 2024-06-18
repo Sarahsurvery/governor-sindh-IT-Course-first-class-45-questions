1eeb74c8421d5c5f4b19bfd8ba03c1f9fb719222
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 23:11:20 +0500

    Add files via upload

---
+++ q34 favourite pizza/app.js
@@ -0,0 +1,8 @@
+var pizza = ["veg pizza", "cheese pizza", "bbq pizza"];
+for (var i = 0; i < pizza.length; i++) {
+    console.log(pizza[i]);
+}
+for (var i = 0; i < pizza.length; i++) {
+    console.log("I eat pizza ".concat(pizza[i]));
+}
+console.log(" Pizza comes in a different variety of flavors ");
---
+++ q34 favourite pizza/app.ts
@@ -0,0 +1,11 @@
+let pizza = ["veg pizza", "cheese pizza", "bbq pizza"];
+
+for(let i=0; i < pizza.length; i++){
+    console.log(pizza[i])
+}
+
+for(let i=0; i < pizza.length; i++){
+    console.log(`I eat pizza ${pizza[i]}`);
+}
+
+console.log(` Pizza comes in a different variety of flavors `);
