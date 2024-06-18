42d3924becc2878e41f66694ab2e965aaaff3511
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 23:03:33 +0500

    Add files via upload

---
+++ q31 No user/app.js
@@ -0,0 +1,9 @@
+var users = ["bilal", "hira", "faizan", "admin"];
+if (users.length === 0) {
+    console.log(" We need to find some user! ");
+}
+//rempve all usser name from array
+else {
+    users = [];
+    console.log("all user have been removed   " + users.length);
+}
---
+++ q31 No user/app.ts
@@ -0,0 +1,13 @@
+let users : string [] = ["bilal", "hira", "faizan", "admin"]
+
+
+    if (users.length === 0 ){
+        console.log(" We need to find some user! ");
+    }
+
+//rempve all usser name from array
+else{
+    users =[]
+    
+    console.log("all user have been removed   " + users.length);
+}
