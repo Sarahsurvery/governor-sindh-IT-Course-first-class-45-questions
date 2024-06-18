127965a331c4329ecf9f73b53d0c7e96c1667312
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 23:38:38 +0500

    Add files via upload

---
+++ q43 unchange megician/app.js
@@ -0,0 +1,13 @@
+var megician = ["don", "sabir ali", "ayaz chohuan"];
+function make_great(megician) {
+    var great_megician = [];
+    megician.forEach(function (megician) {
+        great_megician.push("".concat(megician, " the great"));
+    });
+    return greatmegician;
+}
+var greatmegician = make_great(megician.slice());
+console.log("orignal megician");
+//show_megician(megician);
+console.log("great megician");
+//show_megician(greatmegician);
---
+++ q43 unchange megician/app.ts
@@ -0,0 +1,18 @@
+let megician:string[]=["don","sabir ali","ayaz chohuan"];
+
+function make_great(megician:string[]):string[]{
+
+    let great_megician=[];
+    megician.forEach(megician=>{
+
+        great_megician.push(`${megician} the great`);
+    });
+
+    return greatmegician
+}
+let greatmegician=make_great(megician.slice());
+
+console.log("orignal megician");
+//show_megician(megician);
+console.log("great megician");
+//show_megician(greatmegician);
