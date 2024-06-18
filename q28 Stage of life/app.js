bd5dbfa53549d034e265159646547da4b781c03d
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 22:03:24 +0500

    Add files via upload

---
+++ q28 Stage of life/app.js
@@ -0,0 +1,19 @@
+var age = 15;
+if (age < 2) {
+    console.log("the person is a baby");
+}
+else if (age >= 5 && age < 5) {
+    console.log("person is a toddler");
+}
+else if (age >= 4 && age < 15) {
+    console.log("person is a kid");
+}
+else if (age >= 15 && age < 20) {
+    console.log("person is a teenager");
+}
+else if (age >= 20 && age < 70) {
+    console.log("person is a adult");
+}
+else {
+    console.log("person is an elder");
+}
