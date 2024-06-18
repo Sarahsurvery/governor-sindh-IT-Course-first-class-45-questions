158ba4da9caf2aed883f3f1c268631bc822b0fb0
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 23:33:48 +0500

    Add files via upload

---
+++ q41 magician/app.js
@@ -0,0 +1,7 @@
+var magician = ["don", "herry houdini", "die vernon", "sabir ali", "afzal afridi", "zia chohuan"];
+function show_magician(magician) {
+    magician.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+show_magician(magician);
---
+++ q41 magician/app.ts
@@ -0,0 +1,12 @@
+let magician: string[]=["don","herry houdini","die vernon","sabir ali","afzal afridi","zia chohuan"];
+
+function show_magician(magician:string[]){
+
+    magician.forEach(magician=>{
+
+        console.log(magician);
+    }
+)
+
+}
+show_magician(magician)
