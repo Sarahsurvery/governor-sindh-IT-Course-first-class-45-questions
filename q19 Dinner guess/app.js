b1cd2f497ffe837233f039082aff37d5f0e25e2d
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 20:36:15 +0500

    Add files via upload

---
+++ q19 Dinner guess/app.js
@@ -0,0 +1,3 @@
+import { guest_list } from '../q14 Guest_list/app.js';
+console.log("n\printing number of guess invited");
+console.log(`we had finally invited ${guest_list.length} from excersice 14 `);
