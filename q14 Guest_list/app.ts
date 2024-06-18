9335ae215a9f8b6da8736419f6a9ae00e9a29c0d
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 20:20:06 +0500

    Add files via upload

---
+++ q14 Guest_list/app.ts
@@ -0,0 +1,8 @@
+//exercise 14
+//guest_list
+
+let guest_list: string [] = ['waniya','hira','faizan', 'bilal'];
+for (let i =0; i < guest_list.length; i++){
+    console.log(' Respected friends ' + guest_list [i] + ',\n please come to my party at home');
+}
+export{guest_list};
