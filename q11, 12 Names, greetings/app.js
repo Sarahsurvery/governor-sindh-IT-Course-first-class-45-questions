96cd399bc7e2cdaca8e196aff73b2dd1366ac888
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 20:10:24 +0500

    Add files via upload

---
+++ q11, 12 Names, greetings/app.js
@@ -0,0 +1,10 @@
+var member = ["faizan", "bilal", "hira", "sarah"];
+for (var i = 0; i < member.length; i++) {
+    console.log(member[i]);
+}
+//exercie 12
+var members = ["ahmed", "umer", "waniya", "asiya"];
+var messages = "tomorrow you have to come to my home:";
+for (var i = 0; i < members.length; i++) {
+    console.log(messages + members[i]);
+}
---
+++ q11, 12 Names, greetings/app.ts
@@ -0,0 +1,14 @@
+//exercise 11
+let member : string [] = ["faizan","bilal","hira","sarah"];
+    
+    for(let i = 0; i < member.length; i++){
+    console.log(member[i]);
+}
+//exercie 12
+
+let members : string[] = ["ahmed","umer","waniya","asiya"];
+let messages: string = "tomorrow you have to come to my home:"
+
+    for (let i = 0; i < members.length; i++){
+    console.log(messages+members[i]);
+}
