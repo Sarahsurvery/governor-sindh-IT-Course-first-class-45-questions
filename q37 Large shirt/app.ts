e101a0bd8ddcc79c9d92f805a3d8d85f5d81d32c
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 23:20:18 +0500

    Add files via upload

---
+++ q37 Large shirt/app.ts
@@ -0,0 +1,7 @@
+function make_shirt(size: string = "large", message: string = "I love typescrit"){
+    console.log(`Making a ${size} t-shirt with the message "${message}" printed on it`)
+}
+
+make_shirt();
+make_shirt("medium");
+make_shirt("small","I love Karachi");
