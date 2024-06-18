97b50f59705dac00a7f204899a460a4637d81e65
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-18 20:14:39 +0500

    Add files via upload

---
+++ q13 Own array/app.ts
@@ -0,0 +1,10 @@
+//exercise 13
+
+let transportation : string []=["civic","bike","bus","train"];
+for (let i =0; i< transportation.length; i++){
+    console.log('I would like to own,' + transportation[i]);
+}
+
+
+
+
